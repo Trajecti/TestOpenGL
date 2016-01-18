e67dc2cd3eb2c101dbc842db110d98ac8e4054b1
Traject <manu1297x@gmail.com>
2016-01-17 19:21:08 -0500

    TUT 3.5 Matrix Perspective

--- TestOpenGL/TestOpenGL/TestOpenGL.cpp
+++ TestOpenGL/TestOpenGL/TestOpenGL.cpp
@@ -15,12 +15,109 @@ const int SCREEN_HEIGHT = 480;
 // ids for program and vertex positions
 GLuint positionBufferObject;
 GLuint theProgram;
-GLuint vao, elapsedTimeUniform;
+GLuint vao, offsetUniform, frustumScaleUnif, zNearUnif , zFarUnif;
 
-const float vertexPositions[] = {
-	0.25f, 0.25f, 0.0f, 1.0f,
-	0.25f, -0.25f, 0.0f, 1.0f,
-	-0.25f, -0.25f, 0.0f, 1.0f,
+const float vertexData[] = {
+	0.25f,  0.25f, -1.25f, 1.0f,
+	0.25f, -0.25f, -1.25f, 1.0f,
+	-0.25f,  0.25f, -1.25f, 1.0f,
+
+	0.25f, -0.25f, -1.25f, 1.0f,
+	-0.25f, -0.25f, -1.25f, 1.0f,
+	-0.25f,  0.25f, -1.25f, 1.0f,
+
+	0.25f,  0.25f, -2.75f, 1.0f,
+	-0.25f,  0.25f, -2.75f, 1.0f,
+	0.25f, -0.25f, -2.75f, 1.0f,
+
+	0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f,  0.25f, -2.75f, 1.0f,
+	-0.25f, -0.25f, -2.75f, 1.0f,
+
+	-0.25f,  0.25f, -1.25f, 1.0f,
+	-0.25f, -0.25f, -1.25f, 1.0f,
+	-0.25f, -0.25f, -2.75f, 1.0f,
+
+	-0.25f,  0.25f, -1.25f, 1.0f,
+	-0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f,  0.25f, -2.75f, 1.0f,
+
+	0.25f,  0.25f, -1.25f, 1.0f,
+	0.25f, -0.25f, -2.75f, 1.0f,
+	0.25f, -0.25f, -1.25f, 1.0f,
+
+	0.25f,  0.25f, -1.25f, 1.0f,
+	0.25f,  0.25f, -2.75f, 1.0f,
+	0.25f, -0.25f, -2.75f, 1.0f,
+
+	0.25f,  0.25f, -2.75f, 1.0f,
+	0.25f,  0.25f, -1.25f, 1.0f,
+	-0.25f,  0.25f, -1.25f, 1.0f,
+
+	0.25f,  0.25f, -2.75f, 1.0f,
+	-0.25f,  0.25f, -1.25f, 1.0f,
+	-0.25f,  0.25f, -2.75f, 1.0f,
+
+	0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f, -0.25f, -1.25f, 1.0f,
+	0.25f, -0.25f, -1.25f, 1.0f,
+
+	0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f, -0.25f, -2.75f, 1.0f,
+	-0.25f, -0.25f, -1.25f, 1.0f,
+
+
+
+
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+
+
 };
 
 
@@ -35,7 +132,7 @@ void InitializeVertexBuffer() {
 	glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
 
 	//allocates  to OpenGl size of memory of data and copy data to it
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STREAM_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
 
 	//unbinds buffer using 0 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -50,6 +147,13 @@ void init()
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
+
+	//enables face culling to not draw sides we cannot see
+	glEnable(GL_CULL_FACE);
+	//culls the back
+	glCullFace(GL_BACK);
+	//defines clockwise winding order to be the front
+	glFrontFace(GL_CW);
 }
 
 
@@ -57,8 +161,8 @@ void init()
 void InitializeProgram() {
 	//creates a list of shader objects we are looking for
 	std::vector<GLuint> shaderList;
-	std::string strVertexShader = "data/calcOffset.vert";
-	std::string strFragmentShader = "data/calcColor.frag";
+	std::string strVertexShader = "data/ManualPerspective.vert";
+	std::string strFragmentShader = "data/color.frag";
 
 	std::string test = Framework::LoadFile(strFragmentShader);
 	shaderList.push_back(Framework::LoadShader(GL_VERTEX_SHADER, Framework::LoadFile(strVertexShader)));
@@ -66,14 +170,17 @@ void InitializeProgram() {
 
 	theProgram = Framework::CreateProgram(shaderList);
 
-	elapsedTimeUniform = glGetUniformLocation(theProgram, "time");
+	offsetUniform = glGetUniformLocation(theProgram, "offset");
 
-	GLuint loopDurationUnf = glGetUniformLocation(theProgram, "loopDuration");
-	GLuint fragLoopDurUnf = glGetUniformLocation(theProgram, "fragLoopDuration");
+	frustumScaleUnif = glGetUniformLocation(theProgram, "frustumScale");
+	zNearUnif = glGetUniformLocation(theProgram, "zNear");
+	zFarUnif = glGetUniformLocation(theProgram, "zFar");
 
 	glUseProgram(theProgram);
-	glUniform1f(loopDurationUnf, 5.0f);
-	glUniform1f(fragLoopDurUnf, 10.0f);
+
+	glUniform1f(frustumScaleUnif, 1.0f);
+	glUniform1f(zNearUnif, 1.0f);
+	glUniform1f(zFarUnif, 3.0f);
 	glUseProgram(0);
 	//deletes shaders after use
 	//std::for_each(shaderList.begin(), shaderList.end(), glDeleteShader);
@@ -93,24 +200,25 @@ void display() {
 	//uses the program created
 	glUseProgram(theProgram);
 
-	glUniform1f(elapsedTimeUniform, glutGet(GLUT_ELAPSED_TIME) /1000.0f);
+	glUniform2f(offsetUniform, 0.5f, 0.5f);
 
+	size_t colorData = sizeof(vertexData) / 2;
 	//binds array buffer the buffer object created and passes info on attribute array index 0
 	glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
 	glEnableVertexAttribArray(0);
-	//glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
 
 	// the void (void*48) represents how much to offset in the array by 
 	// 4 (the size of a float) * 4 (the number of floats in a vec4) 
 	//* 3 (the number of vec4's in the position data).
-	//glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*) 48);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)colorData);
 	
 	//draws triangles
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 
 	glDisableVertexAttribArray(0);
-	//glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(1);
 	glUseProgram(0);
 
 	glutSwapBuffers();
